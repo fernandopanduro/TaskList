b7d62b1c6199cc7113157df4ef2eb513bd47d0d6
Fernando Panduro <fernandogp2003pro@gmail.com>
2022-10-06 17:20:18 -0600

    completado estilos del formulario

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import './App.css';
+import Task from './components/Task';
+import FormTask from './components/Form';
 
 function App() {
   return (
     <div className="App">
 
-      <section>
-        <h1>Task List</h1>
-
-        <div className='contenedor contenedor-tasklist'>
-
+      <div className='box'>
+        <div className='contenedor-tasklist'>
+          <h1>Task List</h1>
+          
         </div>
-      </section>
+      </div>
       
     </div>
   );
